83dcb9df9ee8eed4ea2a6429fb025e6192993288
albertoiur <ruialduol@gmail.com>
2019-03-25 15:35:27 +0000

    Update-v2 - 25/03/2019

--- tcp_client.py
+++ tcp_client.py
@@ -1,31 +1,10 @@
 import socket
-import threading
 
-target_host="0.0.0.0"
-target_port=9999
+target_host="127.0.0.1"
+target_port=80
 
-#Criar o cliente socket
-client=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+client.sendto("AAABBBCCC", (target_host,target_port))
+data, addr=client.recvfrom(4096)
 
-#Connectar o cliente
-client.bind((target_host,target_port))
-client.listen(5)
-
-print("[*] Escutar em {0}:{1}".format(target_host,target_port))
-
-def handle_client(client):
-    request=client.recv(1024)
-    print("[*] Recebido: {0}".format(request))
-
-    client.send("Hello world")
-    client.close()
-
-while True:
-    client, addr = client.accept()
-    #accept_connection = ()
-    print("[*] Accept connection em {0}:{1}".format(addr[0],addr[1]))
-    #print(“Accept connection em {0}:{1}”.format(addr[0], addr[1]))
-    # Coloca a thread de cliente em ação para tratar
-    # os dados de   entrada
-    client_handler = threading.Thread(target=handle_client, args=(client, ))
-    client_handler.start()
+print(data)
