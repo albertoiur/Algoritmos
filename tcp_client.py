e72fce38a169f16fbf34c95aeeae46e5f78771ba
albertoiur <ruialduol@gmail.com>
2019-03-25 15:12:41 +0000

    Update - 25/03/2019

--- tcp_client.py
+++ tcp_client.py
@@ -1,19 +1,31 @@
 import socket
+import threading
 
-target_host="www.google.com"
-target_port=80
+target_host="0.0.0.0"
+target_port=9999
 
 #Criar o cliente socket
 client=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 #Connectar o cliente
-client.connect((target_host,target_port))
+client.bind((target_host,target_port))
+client.listen(5)
 
-#Enviar dados
-client.send("GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
+print("[*] Escutar em {0}:{1}".format(target_host,target_port))
 
-#Recebe dados
+def handle_client(client):
+    request=client.recv(1024)
+    print("[*] Recebido: {0}".format(request))
 
-response=client.recv(4096)
+    client.send("Hello world")
+    client.close()
 
-print(response)
+while True:
+    client, addr = client.accept()
+    #accept_connection = ()
+    print("[*] Accept connection em {0}:{1}".format(addr[0],addr[1]))
+    #print(“Accept connection em {0}:{1}”.format(addr[0], addr[1]))
+    # Coloca a thread de cliente em ação para tratar
+    # os dados de   entrada
+    client_handler = threading.Thread(target=handle_client, args=(client, ))
+    client_handler.start()
